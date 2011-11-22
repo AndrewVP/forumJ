431668d481c6af7da05137d2be29d4e11d359c56
an.pogrebnyak@gmail.com <an.pogrebnyak@gmail.com@f119759f-f751-4154-266f-3b7cfe25484e>
2011-11-22 13:38:40 +0000

    refactoring

--- trunk/src/main/java/org/forumj/db/dao/FJUserDao.java
+++ trunk/src/main/java/org/forumj/db/dao/FJUserDao.java
@@ -11,95 +11,146 @@ import org.forumj.db.entity.*;
 
 public class FJUserDao extends FJDao {
 
-   public IUser read(Long userId, String password, boolean firstPassword) throws ConfigurationException, SQLException{
-      String query="SELECT * FROM users WHERE id=" + userId.toString();
-      String add = "";
+   public IUser read(Long userId, String password, boolean firstPassword) throws ConfigurationException, SQLException, IOException{
+      IUser result = null;
+      String query = null;
       if(firstPassword){
-         add = " AND pass='" + password + "'";
+         query = getReadUserByFirstPasswordQuery();
       }else{
-         add = " AND pass2='" + password + "'";
+         query = getReadUserBySecondPasswordQuery();
       }
-      return loadUser(query + add, firstPassword);
-   }
-
-   public IUser read(String nick, String password, Boolean firstPassword) throws ConfigurationException, SQLException{
-      String query="SELECT * FROM users WHERE nick='" + nick + "' AND pass='" + password + "'";
-      String add = "";
-      if(firstPassword){
-         add = " AND pass='" + password + "'";
-      }else{
-         add = " AND pass2='" + password + "'";
-      }
-      return loadUser(query + add, firstPassword);
-   }
-   
-   public User read(Long userId) throws ConfigurationException, SQLException{
-      String query="SELECT * FROM users WHERE id=" + userId.toString();
-      return loadUser(query, null);
-   }
-   
-   public User read(String nick) throws ConfigurationException, SQLException{
-      String query="SELECT * FROM users WHERE nick=\"" + nick + "\"";
-      return loadUser(query, null);
-   }
-   
-   public User readByMail(String mail) throws ConfigurationException, SQLException{
-      String query="SELECT * FROM users WHERE mail=\"" + mail + "\"";
-      return loadUser(query, null);
-   }
-   
-   private User loadUser(String sql, Boolean firstPassword) throws ConfigurationException, SQLException{
-      User result = null;
       Connection conn = null;
-      Statement st = null;
+      PreparedStatement st = null;
       try {
          conn = getConnection();
-         st = conn.createStatement();
-         ResultSet rs = st.executeQuery(sql);
-         if (rs.next()){
-            result = new User();
-            result.setId(rs.getLong("id"));
-            result.setNick(rs.getString("nick"));
-            result.setPass(rs.getString("pass"));
-            result.setPass2(rs.getString("pass2"));
-            result.setPp(rs.getInt("pp_def"));
-            result.setPt(rs.getInt("pt_def"));
-            result.setView(rs.getInt("view_def"));
-            result.setTimeZone(rs.getInt("fd_timezone"));
-            result.setWantSeeAvatars(rs.getInt("v_avatars") > 0);
-            result.setBan(rs.getInt("ban"));
-            result.setAvatar(rs.getString("avatar"));
-            result.setAvatarApproved(rs.getInt("ok_avatar") > 0);
-            result.setShowAvatar(rs.getInt("s_avatar") > 0);
-            result.setName(rs.getString("name"));
-            result.setFam(rs.getString("fam"));
-            result.setSex(rs.getByte("sex"));
-            result.setBith(rs.getDate("bith"));
-            result.setReg(rs.getDate("reg"));
-            result.setShowMail(rs.getBoolean("smail"));
-            result.setShowName(rs.getBoolean("sname"));
-            result.setCity(rs.getString("city"));
-            result.setShowCity(rs.getBoolean("scity"));
-            result.setCountry(rs.getString("country"));
-            result.setShowCountry(rs.getBoolean("scountry"));
-            result.setShowSex(rs.getBoolean("ssex"));
-            result.setShowBithday(rs.getBoolean("sbith"));
-            result.setIcq(rs.getString("icq"));
-            result.setShowIcq(rs.getBoolean("sicq"));
-            result.setHideIp(rs.getBoolean("h_ip"));
-            result.setLanguge(rs.getInt("lang"));
-            result.setFooter(rs.getString("footer"));
-            result.setActivateCode(rs.getInt("activate_code"));
-            result.setIsActive(rs.getInt("is_active")>1);
-            result.setEmail(rs.getString("mail"));
-         }
+         st = conn.prepareStatement(query) ;
+         st.setLong(1, userId);
+         st.setString(2, password);
+         ResultSet rs = st.executeQuery();
+         result = loadUser(rs);
       }finally{
          readFinally(conn, st);
       }
       return result;
    }
 
-   public void update(User user) throws IOException, ConfigurationException, SQLException {
+   public IUser read(String nick, String password, Boolean firstPassword) throws ConfigurationException, SQLException, IOException{
+      IUser result = null;
+      String query = null;
+      if(firstPassword){
+         query = getReadUserByNickAndFirstPasswordQuery();
+      }else{
+         query = getReadUserByNickAndSecondPasswordQuery();
+      }
+      Connection conn = null;
+      PreparedStatement st = null;
+      try {
+         conn = getConnection();
+         st = conn.prepareStatement(query) ;
+         st.setString(1, nick);
+         st.setString(2, password);
+         ResultSet rs = st.executeQuery();
+         result = loadUser(rs);
+      }finally{
+         readFinally(conn, st);
+      }
+      return result;
+   }
+   
+   public IUser read(Long userId) throws ConfigurationException, SQLException, IOException{
+      IUser result = null;
+      String query = getReadUserByIdQuery();
+      Connection conn = null;
+      PreparedStatement st = null;
+      try {
+         conn = getConnection();
+         st = conn.prepareStatement(query) ;
+         st.setLong(1, userId);
+         ResultSet rs = st.executeQuery();
+         result = loadUser(rs);
+      }finally{
+         readFinally(conn, st);
+      }
+      return result;
+   }
+   
+   public IUser read(String nick) throws ConfigurationException, SQLException, IOException{
+      IUser result = null;
+      String query = getReadUserByNickQuery();
+      Connection conn = null;
+      PreparedStatement st = null;
+      try {
+         conn = getConnection();
+         st = conn.prepareStatement(query) ;
+         st.setString(1, nick);
+         ResultSet rs = st.executeQuery();
+         result = loadUser(rs);
+      }finally{
+         readFinally(conn, st);
+      }
+      return result;
+   }
+   
+   public IUser readByMail(String mail) throws ConfigurationException, SQLException, IOException{
+      IUser result = null;
+      String query = getReadUserByMailQuery();
+      Connection conn = null;
+      PreparedStatement st = null;
+      try {
+         conn = getConnection();
+         st = conn.prepareStatement(query) ;
+         st.setString(1, mail);
+         ResultSet rs = st.executeQuery();
+         result = loadUser(rs);
+      }finally{
+         readFinally(conn, st);
+      }
+      return result;
+   }
+   
+   private User loadUser(ResultSet rs) throws ConfigurationException, SQLException{
+      User result = null;
+      if (rs.next()){
+         result = new User();
+         result.setId(rs.getLong("id"));
+         result.setNick(rs.getString("nick"));
+         result.setPass(rs.getString("pass"));
+         result.setPass2(rs.getString("pass2"));
+         result.setPp(rs.getInt("pp_def"));
+         result.setPt(rs.getInt("pt_def"));
+         result.setView(rs.getInt("view_def"));
+         result.setTimeZone(rs.getInt("fd_timezone"));
+         result.setWantSeeAvatars(rs.getInt("v_avatars") > 0);
+         result.setBan(rs.getInt("ban"));
+         result.setAvatar(rs.getString("avatar"));
+         result.setAvatarApproved(rs.getInt("ok_avatar") > 0);
+         result.setShowAvatar(rs.getInt("s_avatar") > 0);
+         result.setName(rs.getString("name"));
+         result.setFam(rs.getString("fam"));
+         result.setSex(rs.getByte("sex"));
+         result.setBith(rs.getDate("bith"));
+         result.setReg(rs.getDate("reg"));
+         result.setShowMail(rs.getBoolean("smail"));
+         result.setShowName(rs.getBoolean("sname"));
+         result.setCity(rs.getString("city"));
+         result.setShowCity(rs.getBoolean("scity"));
+         result.setCountry(rs.getString("country"));
+         result.setShowCountry(rs.getBoolean("scountry"));
+         result.setShowSex(rs.getBoolean("ssex"));
+         result.setShowBithday(rs.getBoolean("sbith"));
+         result.setIcq(rs.getString("icq"));
+         result.setShowIcq(rs.getBoolean("sicq"));
+         result.setHideIp(rs.getBoolean("h_ip"));
+         result.setLanguge(rs.getInt("lang"));
+         result.setFooter(rs.getString("footer"));
+         result.setActivateCode(rs.getInt("activate_code"));
+         result.setIsActive(rs.getInt("is_active")>1);
+         result.setEmail(rs.getString("mail"));
+      }
+      return result;
+   }
+
+   public void update(IUser user) throws IOException, ConfigurationException, SQLException {
       String query = getUpdateUserQuery();
       Connection conn = null;
       PreparedStatement st = null;
@@ -149,7 +200,7 @@ public class FJUserDao extends FJDao {
       }
    }
    
-   public void create(User user) throws SQLException, ConfigurationException, IOException{
+   public void create(IUser user) throws SQLException, ConfigurationException, IOException{
       String query = getUpdateUserQuery();
       Connection conn = null;
       PreparedStatement st = null;
