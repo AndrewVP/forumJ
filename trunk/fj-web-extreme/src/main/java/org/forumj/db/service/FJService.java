aafa3c0580c882f2daaaefe4e1b2c2fbf34e9781
an.pogrebnyak@gmail.com <an.pogrebnyak@gmail.com@f119759f-f751-4154-266f-3b7cfe25484e>
2011-11-29 18:03:34 +0000

    IndexService

---
+++ trunk/fj-web-extreme/src/main/java/org/forumj/db/service/FJService.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright Andrew V. Pogrebnyak
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.forumj.db.service;
+
+import org.forumj.db.dao.*;
+
+/**
+ * 
+ * @author <a href="mailto:an.pogrebnyak@gmail.com">Andrew V. Pogrebnyak</a>
+ */
+public class FJService {
+   
+   private static final FJForumDao forumDao = new FJForumDao();
+
+   private static final FJPostDao postDao = new FJPostDao();
+
+   private static final FJThreadDao threadDao = new FJThreadDao();
+
+   private static final FJMailDao mailDao = new FJMailDao();
+
+   private static final FJInterfaceDao interfaceDao = new FJInterfaceDao();
+
+   private static final FJFolderDao folderDao = new FJFolderDao();
+   
+   private static final FJActionDao actionDao = new FJActionDao();
+   
+   
+   /**
+    * @return the actiondao
+    */
+   public static FJActionDao getActiondao() {
+      return actionDao;
+   }
+
+   /**
+    * @return the folderdao
+    */
+   public static FJFolderDao getFolderDao() {
+      return folderDao;
+   }
+
+   /**
+    * @return the interfacedao
+    */
+   public static FJInterfaceDao getInterfaceDao() {
+      return interfaceDao;
+   }
+
+   /**
+    * @return the maildao
+    */
+   public static FJMailDao getMailDao() {
+      return mailDao;
+   }
+
+   /**
+    * @return the threaddao
+    */
+   public static FJThreadDao getThreadDao() {
+      return threadDao;
+   }
+
+   /**
+    * @return the postdao
+    */
+   public static FJPostDao getPostDao() {
+      return postDao;
+   }
+
+   /**
+    * @return the forumdao
+    */
+   public static FJForumDao getForumDao() {
+      return forumDao;
+   }
+
+}
---
+++ trunk/fj-web-extreme/src/main/java/org/forumj/db/service/IndexService.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright Andrew V. Pogrebnyak
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.forumj.db.service;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.forumj.common.db.entity.*;
+import org.forumj.db.entity.*;
+import org.forumj.tool.LocaleString;
+
+/**
+ * 
+ * @author <a href="mailto:an.pogrebnyak@gmail.com">Andrew V. Pogrebnyak</a>
+ */
+public class IndexService extends FJService{
+   
+   /**
+    * Возвращает id последнего поста в форуме
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static Long getLastPostId() throws ConfigurationException, SQLException, IOException{
+      return getPostDao().getLastPostId(); 
+   }
+   
+   /**
+    * Возвращает id последней ветки в форуме
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static Long getMaxThreadId() throws ConfigurationException, SQLException, IOException{
+      return getThreadDao().getMaxThreadId();
+   }
+   
+   /**
+    * 
+    * @param viewId
+    * @param nfirstpost
+    * @param locale
+    * @param user
+    * @param ignorList
+    * @return
+    * @throws SQLException
+    * @throws ConfigurationException
+    */
+   public static FJThreads getThreads(Long viewId, long nfirstpost, LocaleString locale, IUser user, List<Ignor> ignorList) throws SQLException, ConfigurationException{
+      return getThreadDao().getThreads(viewId, nfirstpost, locale, user, ignorList);
+   }
+   
+   /**
+    * 
+    * @param idUser
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static int getNewMailCount(Long idUser) throws ConfigurationException, SQLException, IOException{
+      return getMailDao().getNewMailCount(idUser);
+   }
+
+   /**
+    * 
+    * @param idView
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static String getViewName(Long idView) throws ConfigurationException, SQLException, IOException{
+      return getInterfaceDao().getViewName(idView);
+   }
+   
+   /**
+    * 
+    * @param idUser
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static List<IFJInterface> getViewsArray(Long idUser) throws ConfigurationException, SQLException, IOException{
+      return getInterfaceDao().getViewsArray(idUser);
+   }
+   
+   /**
+    * 
+    * @param idUser
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static List<IFJFolder> getUserFolders(Long idUser) throws ConfigurationException, SQLException, IOException{
+      return getFolderDao().getUserFolders(idUser);
+   }
+   
+   /**
+    * 
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static List<IUser> getUsersArray() throws ConfigurationException, SQLException, IOException{
+      return getActiondao().getUsersArray();
+   }
+   
+   /**
+    * 
+    * @return
+    * @throws ConfigurationException
+    * @throws SQLException
+    * @throws IOException
+    */
+   public static int getGuestsAmount() throws ConfigurationException, SQLException, IOException{
+      return getActiondao().getGuestsAmount();
+   }
+   
+}
