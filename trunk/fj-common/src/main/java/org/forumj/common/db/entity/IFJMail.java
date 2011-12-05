887493ab3e0e93b0c7b507cb8fa3257eca5969ef
an.pogrebnyak@gmail.com <an.pogrebnyak@gmail.com@f119759f-f751-4154-266f-3b7cfe25484e>
2011-12-05 19:03:32 +0000

    ControlService

--- trunk/fj-common/src/main/java/org/forumj/common/db/entity/IFJMail.java
+++ trunk/fj-common/src/main/java/org/forumj/common/db/entity/IFJMail.java
@@ -9,6 +9,8 @@
  */
 package org.forumj.common.db.entity;
 
+import java.util.Date;
+
 /**
  *
  * @author <a href="mailto:an.pogrebnyak@gmail.com">Andrew V. Pogrebnyak</a>
@@ -28,4 +30,26 @@ public interface IFJMail {
 
    public static final String RECEIVER_NICK_FIELD_NAME = "rcvr_nick";
    public static final String SENDER_NICK_FIELD_NAME = "sndr_nick";
+   public abstract void setDeletedByReceiver(Integer deletedByReceiver);
+   public abstract Integer getDeletedByReceiver();
+   public abstract void setDeletedBySender(Integer deletedBySender);
+   public abstract Integer getDeletedBySender();
+   public abstract void setBody(String body);
+   public abstract String getBody();
+   public abstract void setSubject(String subject);
+   public abstract String getSubject();
+   public abstract void setReadDate(Date readDate);
+   public abstract Date getReadDate();
+   public abstract void setReceiveDate(Date receiveDate);
+   public abstract Date getReceiveDate();
+   public abstract void setSentDate(Date sentDate);
+   public abstract Date getSentDate();
+   public abstract void setCreateDate(Date createDate);
+   public abstract Date getCreateDate();
+   public abstract void setId(Long id);
+   public abstract Long getId();
+   public abstract void setReceiver(IUser receiver);
+   public abstract IUser getReceiver();
+   public abstract void setSender(IUser sender);
+   public abstract IUser getSender();
 }
