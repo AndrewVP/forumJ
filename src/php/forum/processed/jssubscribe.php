d2ea3c2f7a8b4c05ac62c9a90251506f7d60586b
an.pogrebnyak <an.pogrebnyak@gmail.com>
2012-11-05 21:36:40 +0000

    good bye, php

--- src/php/forum/processed/jssubscribe.php
+++
@@ -1,9 +0,0 @@
-<script type='text/javascript'>
-   function subscribe() {
-      for (i=0; i < document.forms.length; i++) {
-         if (document.forms[i].id=='subs'){
-            document.forms[i].submit();
-         }
-      }
-   }
-</script>                  
--- src/php/forum/processed/pass1.php
+++
@@ -1,3 +0,0 @@
-<script language="PHP">
-   mail("a_2005@bigmir.net", "Вы забыли пароль :)","Ваш пароль на форуме Дилетант:\n" , "From: diletant@diletant.com.ua\nReply-To: pass@diletant.com.ua\nX-Mailer: PHP/" . phpversion());
-</script>
