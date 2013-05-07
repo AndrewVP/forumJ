954a0b7bdf940df37c3c76d757aa2a00de95a12c
an.pogrebnyak@gmail.com <an.pogrebnyak@gmail.com@f119759f-f751-4154-266f-3b7cfe25484e>
2013-05-07 22:09:34 +0000

    service module added

---
+++ trunk/fj-business/src/main/java/org/forumj/service/InterfaceServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Andrew V. Pogrebnyak
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.forumj.service;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.forumj.common.db.entity.*;
+import org.forumj.common.db.service.InterfaceService;
+
+/**
+ * 
+ * @author <a href="mailto:an.pogrebnyak@gmail.com">Andrew V. Pogrebnyak</a>
+ */
+public class InterfaceServiceImpl extends FJService implements InterfaceService {
+
+   public boolean isInterfaceContainsFolder(long interfaceId, long folderId, IUser user) throws IOException, ConfigurationException, SQLException{
+      return getInterfaceDao().isInterfaceContainsFolder(interfaceId, folderId, user);
+   }
+   
+   public void addFolder(long interfaceId, long folderId, IUser user, Connection connection) throws ConfigurationException, SQLException, IOException{
+      getInterfaceDao().addFolder(interfaceId, folderId, user, connection);
+   }
+   
+   public void deleteInterface(long interfaceId, IUser user) throws ConfigurationException, SQLException, IOException{
+      getInterfaceDao().delete(interfaceId, user);
+   }
+   
+
+   public List<IFJInterface> findAllInterfaces(IUser user) throws IOException, SQLException, ConfigurationException{
+      return getInterfaceDao().findAll(user);
+   }
+   
+   public IFJInterface findInterface(IUser user, Long id) throws IOException, SQLException, ConfigurationException{
+      return getInterfaceDao().find(user, id);
+   }
+
+   public boolean isExist(String name, IUser user) throws ConfigurationException, SQLException, IOException {
+      return getInterfaceDao().isExist(name, user);
+   }
+
+   public void create(String name, IUser user) throws ConfigurationException, SQLException, IOException {
+      getInterfaceDao().create(name, user);
+   }
+}
+
