ab198275ed2f5e3f8057ccdff8f3c6c8051ea517
Andrey Pogrebniak <andrey.pogrebniak@asg.com>
2017-03-12 16:51:21 +0200

    DB migration part 2: added ignoring of threads

--- fj-db-extreme/src/main/resources/sql/loadLastThreadsIdsNoForum.sql
+++ fj-db-extreme/src/main/resources/sql/loadLastThreadsIdsNoForum.sql
@@ -6,6 +6,8 @@ WHERE
  thread IN (select title from fdtranzit where folder IN ( SELECT folder FROM fdvtranzit WHERE (user = ? OR user = 0) AND view = ?) AND user = ?)
  AND
  thread NOT IN (select id from titles where dock !=0)
+ AND
+ thread NOT IN (select id from titles where auth IN(SELECT ignor FROM ignor WHERE user = ? AND end > now() AND type = 1) order by id)
 order by
  created desc
 limit ?, ?;
